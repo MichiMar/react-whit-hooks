caed9718741f7934f540b059af3599e0edf80188
MichiMar <michimar.alexis@gmail.com>
2020-08-25 18:42:28 -0500

    different ways to style in react

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    const styles = { height: "100px", backgroundColor: "blue", color: "white" };
+    const dynamicBackgroundColor = "blue";
+
+    const styles = {
+      height: "100px",
+      backgroundColor: dynamicBackgroundColor,
+      color: "white",
+    };
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
