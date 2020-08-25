d4837ff530a7d1ef9a536109130800786a2a46c4
MichiMar <michimar.alexis@gmail.com>
2020-08-25 18:24:00 -0500

    updated styles files

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
+    const styles = { height: "100px", backgroundColor: "blue", color: "white" };
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
         <h2>React + React Router</h2>
+
+        <div style={styles}>Testing Style</div>
       </div>
     );
   }
