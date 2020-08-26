36a8ba7e884e99f748ed483082066c7c202dfd9c
MichiMar <michimar.alexis@gmail.com>
2020-08-26 14:16:06 -0500

    updated static images

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+// @ts-ignore
+const logo = require("../../../static/assets/images/binary.png");
+
 export default () => {
   return (
     <div>
+      <img src={logo} />
       <div>Michelle Alexis Rommy Marquez Soto</div>
 
       <div>Home</div>
