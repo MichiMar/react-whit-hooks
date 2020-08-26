1cdc7ec419edc9c6d74338b9947e787194f11eee
MichiMar <michimar.alexis@gmail.com>
2020-08-26 13:47:30 -0500

    organized elements

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
+import NavBar from "./shared/NavBar";
 
-export default class App extends Component {
-  render() {
-    const dynamicBackgroundColor = "blue";
+const App = () => {
+  const dynamicBackgroundColor = "black";
 
-    const styles = {
-      height: "100px",
-      backgroundColor: dynamicBackgroundColor,
-      color: "white",
-    };
+  const styles = {
+    height: "100px",
+    backgroundColor: dynamicBackgroundColor,
+    color: "white",
+  };
 
-    return (
-      <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + React Router</h2>
+  return (
+    <div className="app">
+      <NavBar />
+      <h1>DevCamp React Starter</h1>
+      <h2>React + React Router</h2>
 
-        <div style={styles}>Testing Style</div>
-      </div>
-    );
-  }
-}
+      <div style={styles}>Testing Style</div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/shared/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <div>Michelle Alexis Rommy Marquez Soto</div>
+
+      <div>Home</div>
+
+      <div>Portfolio</div>
+    </div>
+  );
+};
